797c82bf199b6e87aacbd85c5f81bc2138ec1dab
sejdz <agatasage@gmail.com>
2019-06-08 23:32:42 +0200

    poprawka, u mnie działa
    (pędzel + wysyłanie dicom)

--- KWDM_Segm/bin/Debug/ORTHANC/OrthancSend.m
+++ KWDM_Segm/bin/Debug/ORTHANC/OrthancSend.m
@@ -1,13 +1,17 @@
 function [] = OrthancSend(file_path, segmentation, DICOMfilename)
+
     addpath('.\jsonlab');
     URL = 'http://localhost:8042'; %% adres serwera
-
+	
+	% metadane takie same
     info = dicominfo(fullfile(file_path,DICOMfilename));
     info.BodyPartExamined = [info.BodyPartExamined ' Segmentacja'];
     
+    % czytaj segmentacjê
     img = imread(fullfile(file_path, segmentation));  
     dicomwrite(img, fullfile(file_path,[DICOMfilename '_segm.dcm']), info);
 
+    % wyœlij na serwer
     execute = ['!curl -X POST ' URL '/instances --data-binary @' fullfile(file_path,[DICOMfilename '_segm.dcm'])];
     eval(execute);
 end
